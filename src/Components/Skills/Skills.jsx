07707c4e79f24df3fa6594f2f5f98d0c188e1d1e
Bushra Nikhat <bushranik6@gmail.com>
2021-05-01 22:11:22 +0530

    updating skills component

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -13,7 +13,7 @@ const Skills = () => {
             
                 <SkillContent icon="fab fa-html5 mr-3"
                    sub="HTML5"
-                   width={{ width: '85%' }}
+                   width={{ width: '80%' }}
                    />
                    <SkillContent icon="fab fa-css3-alt mr-3"
                    sub="CSS3"
@@ -21,20 +21,28 @@ const Skills = () => {
                    />
                    <SkillContent icon="fab fa-js-square mr-3"
                    sub="JavaScript"
-                   width={{ width: '65%' }}
+                   width={{ width: '55%' }}
                    />
                  
                 <SkillContent icon="fab fa-bootstrap mr-3"
                    sub="Bootstrap"
-                   width={{ width: '75%' }}
+                   width={{ width: '70%' }}
                    />
                    <SkillContent icon="fas fa-laptop-code mr-3"
                    sub="React JS"
-                   width={{ width: '70%' }}
+                   width={{ width: '50%' }}
                    />
-                   <SkillContent icon="fab fa-python mr-3"
-                   sub="Python"
-                   width={{ width: '65%' }}
+                   <SkillContent icon="fas fa-laptop-code mr-3"
+                   sub="Node JS"
+                   width={{ width: '50%' }}
+                   />
+                   <SkillContent icon="fas fa-laptop-code mr-3"
+                   sub="Express JS"
+                   width={{ width: '50%' }}
+                   />
+                   <SkillContent icon="fas fa-database mr-3"
+                   sub="Mongodb"
+                   width={{ width: '50%' }}
                    />   
                   
                   
